ac09509f0ac296c44a6f7739812c1b8114f7f097
Mohamed-art-lab <mohamed.farah@student.moringaschool.com>
2024-02-20 21:26:54 +0300

    updated slider

--- src/components/sliderData.js
+++ src/components/sliderData.js
@@ -1,30 +1,29 @@
-const sliderData = [
-    {
-      image: 'https://www.intrepidtravel.com/adventures/wp-content/uploads/2022/05/Kenya-Nyama-Choma-sharing-plate-traditional-dish-1320191333-ss-800x450-1.jpg',
-      title: 'Discover Authentic Flavors',
-      body: 'Embark on a culinary journey through Africa and savor the rich, diverse tastes that define the continent. Explore traditional dishes that tell stories of heritage and culture.',
-    },
-    {
-      image: 'https://www.willflyforfood.net/wp-content/uploads/2021/06/kenyan-food-chapati.jpg.webp',
-      title: 'Cooking with Passion',
-      body: 'Step into the heart of African kitchens with our recipes curated for enthusiasts and beginners alike. Learn the art of preparing delicious meals that bring people together.',
-    },
-    {
-      image: 'https://mawuafrica.com/cdn/shop/articles/image1_347d0795-3085-4177-b26e-f788495b3fc3.png?v=1689952484',
-      title: 'A Culinary Expedition',
-      body: 'Embark on an adventure to explore the rich tapestry of African cuisine. From savory stews to delectable desserts, discover the diverse and vibrant culinary traditions of the continent.',
-    },
-    {
-      image: 'https://foodinstitute.com/wp-content/uploads/2021/08/African-Cuisine.jpg',
-      title: 'Share Your Culinary Creations',
-      body: 'Join our community of food enthusiasts and share your favorite African recipes. Whether it\'s a family tradition or a personal twist, let the world taste the flavors of your kitchen.',
-    },
-    {
-      image: 'https://migrationology.com/wp-content/uploads/2011/06/best-kenyan-food-dishes.jpg',
-      title: 'Savor Every Bite, Create Memories',
-      body: 'Experience the joy of trying new flavors with every recipe. Make cooking an adventure, and let each dish you create be a celebration of the rich and diverse culinary heritage of Africa.',
-    }
-  ];
-  
-  export default sliderData;
-  
+const bookData = [
+  {
+    image: 'https://cdnattic.atticbooks.co.ke/img/W151592.png',
+    title: 'Explore Captivating Stories',
+    description: 'Embark on a literary journey through diverse genres and discover the enchanting narratives that weave tales of imagination and emotion.',
+  },
+  {
+    image: 'https://cdnattic.atticbooks.co.ke/img/V179936.png',
+    title: 'Reading with Passion',
+    description: 'Step into the worlds created by talented authors with books curated for both avid readers and those new to the magic of literature. Immerse yourself in the art of storytelling.',
+  },
+  {
+    image: 'https://cdnattic.atticbooks.co.ke/img/939572.jpg',
+    title: 'A Literary Expedition',
+    description: 'Embark on an adventure to explore the rich tapestry of literary works. From gripping novels to thought-provoking poetry, discover the diverse and vibrant traditions of storytelling.',
+  },
+  {
+    image: 'https://cdnattic.atticbooks.co.ke/img/R944659.png',
+    title: 'Share Your Literary Discoveries',
+    description: 'Join our community of book enthusiasts and share your favorite reads. Whether it\'s a timeless classic or a hidden gem, let the world explore the pages of your literary adventures.',
+  },
+  {
+    image: 'https://atticbooks.co.ke/images/loading.svg',
+    title: 'Savor Every Word, Create Memories',
+    description: 'Experience the joy of diving into new stories with every book. Make reading an adventure, and let each page you turn be a celebration of the rich and diverse world of literature.',
+  }
+];
+
+export default bookData;
