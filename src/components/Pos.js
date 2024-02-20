a2411e70db069c1421f0f0723db920abf1b73ff3
Moha <moha@users-MacBook-Pro.local>
2024-02-20 23:37:04 +0300

    minor edit

--- src/components/Pos.js
+++ src/components/Pos.js
@@ -1,110 +1,92 @@
-import React, {useState, useEffect} from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import OrderLine from "./OrderLine";
 
-// const customer_id = document.querySelector("#customer_id").value
+function Orders() {
+  const [customers, setCustomers] = useState([]);
+  const [orders, setOrders] = useState([{ item_id: null, price: 0, QTY: 1, line_total: 0 }]);
 
-function Orders(){
+  useEffect(() => {
+    fetch("http://localhost:3001/items/")
+      .then((response) => response.json())
+      .then((data) => setCustomers(data));
+  }, []);
 
-    const [customers, setCustomers] = useState([])
-    
+  const addOrder = (event) => {
+    event.preventDefault();
+    let newOrder = {
+      item_id: null,
+      price: 0,
+      QTY: 1,
+      line_total: 0,
+    };
 
-    useEffect(()=> {
-        fetch(' http://localhost:3001/items/')
-        .then((response)=> response.json())
-        .then( data => setCustomers(data))
-        
-    }, [])
+    const lastOrderItem = orders[orders.length - 1];
+    if (lastOrderItem.item_id === null) {
+      alert("All order inputs are required before adding a new one!");
+      return;
+    }
 
-    const [orders, setOrders] = useState([{
-        item_id: 0,
-        price: 0,
-        QTY: 1,
-        line_total: 0
-    }])
+    setOrders([...orders, newOrder]);
+  };
 
-
-const addOrder = (event) => {
-    event.preventDefault()
-    let newOrder = { 
-        item_id: 0,
-        price: 0,
-        QTY: 1,
-        line_total: 0
-     }
-
-     const lastOrderItem = orders[orders.length - 1]
-     if(lastOrderItem.item_id === null){
-        alert("All order inputs are required before adding a new one!")
-       return
-     }
-
-     setOrders([
-        ...orders,
-        newOrder
-     ])
-
-}
-
-const submitOrders = async(event) => {
-    event.preventDefault()
-    const customer_id = parseInt(document.querySelector("#customer_id").value)
-    if(!customer_id){
-        alert("The customer name is required")
-        return false
+  const submitOrders = async (event) => {
+    event.preventDefault();
+    const customer_id = parseInt(document.querySelector("#customer_id").value);
+    if (!customer_id) {
+      alert("The customer name is required");
+      return false;
     }
     const finalOrders = {
-        orders: orders.map((order)=>{
-            order["customer_id"] = customer_id
-            delete order['price'];
-            return order;
-        })
-    }
- fetch('https://tomashi.loca.lt/orders',{
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-        body: JSON.stringify(finalOrders)
-    })
-    alert(`${finalOrders.orders.length} items added successfully to your order!`)
-    
+      orders: orders.map((order) => {
+        order["customer_id"] = customer_id;
+        delete order["price"];
+        return order;
+      }),
+    };
+    fetch("https://tomashi.loca.lt/orders", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(finalOrders),
+    });
+    alert(`${finalOrders.orders.length} items added successfully to your order!`);
+  };
 
-}
+  const total = orders.reduce((acc, currentValue) => acc + currentValue.line_total, 0);
 
-const total = orders.reduce((acc, currentValue)=> acc + currentValue.line_total, 0);
-
-
-return (
+  return (
     <div className="container">
-        <form className="border w-75 mx-auto" onSubmit={submitOrders}>
-            <img className="mx-auto d-block w-25" src="./images/supermarket.png" alt="logo"/>
-            <div className="col-sm-3 m-auto">
-                <small className="form-label text-muted">Customer Name</small>
-                <select id="customer_id" name="customer_id"  className="form-select form-select-sm">
-                    <option>--Select Customer--</option>
-                    {customers.map((customer, index)=> <option value={customer.id} key={index}>{customer.customer_name}</option>)}
-                </select>
-            </div>
-            {
-                orders.map((order, index)=> {
-                    return <OrderLine order={order} orders={orders} setOrders={setOrders} index={index} key={index}/>
-                })
-            }
-            <div className="d-flex justify-content-between p-4">
-                <button className="btn btn-sm btn-dark" onClick={addOrder}>
-                    <FontAwesomeIcon className="" icon="plus"/>
-                </button>
-                <button className="btn btn-sm btn-outline-dark  text-uppercase">Submit Order</button>
-            </div>
-            <div className="d-flex justify-content-end p-4">
-                <b>Total: Ksh{total}</b>
-            </div>
-        </form>
+      <form className="border w-75 mx-auto" onSubmit={submitOrders}>
+        <img className="mx-auto d-block w-25" src="./images/supermarket.png" alt="logo" />
+        <div className="col-sm-3 m-auto">
+          <small className="form-label text-muted">Customer Name</small>
+          <select id="customer_id" name="customer_id" className="form-select form-select-sm">
+            <option>--Select Customer--</option>
+            {customers.map((customer, index) => (
+              <option value={customer.id} key={index}>
+                {customer.customer_name}
+              </option>
+            ))}
+          </select>
+        </div>
+        {orders.map((order, index) => (
+          <OrderLine key={index} order={order} orders={orders} setOrders={setOrders} index={index} />
+        ))}
+        <div className="d-flex justify-content-between p-4">
+          <button className="btn btn-sm btn-dark" onClick={addOrder}>
+            <FontAwesomeIcon className="" icon="plus" />
+          </button>
+          <button className="btn btn-sm btn-outline-dark text-uppercase">Submit Order</button>
+        </div>
+        <div className="d-flex justify-content-end p-4">
+          <b>Total: Ksh{total}</b>
+        </div>
+      </form>
     </div>
-)
+  );
 }
 
-export default Orders
+export default Orders;
